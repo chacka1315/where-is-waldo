415073564caf6b6006c7f6101adaf235028f5c6d
chacka1315 <chacka131@gmail.com>
2025-12-08 17:48:42 +0000

    Fix the session config.

--- apps/api/src/config/session.js
+++ apps/api/src/config/session.js
@@ -3,19 +3,20 @@ import prisma from '../prisma/prisma.js';
 
 const sessionStore = new PrismaSessionStore(prisma, {
   dbRecordIdIsSessionId: true,
-  checkPeriod: 1000 * 60 * 60 * 24, // 1 day
+  checkPeriod: 1000 * 60 * 60 * 12, // 12 hours
 });
 
 const sessionConfig = {
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
-  },
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  secure: process.env.NODE_ENV === 'production',
   store: sessionStore,
-  sameSite: 'lax',
+
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 1, // 1 day
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+  },
 };
 
 export default sessionConfig;
